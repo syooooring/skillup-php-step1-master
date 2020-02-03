0bd04ed177ccd31becc675290ebae064ad17d8fd
syooooring <syooooring@gmail.com>
2020-02-03 20:28:35 +0900

    Update UserController.php

--- skillup-php-step1-master/src/app/Http/Controllers/UserController.php
+++ skillup-php-step1-master/src/app/Http/Controllers/UserController.php
@@ -1,13 +1,17 @@
 <?php
 namespace App\Http\Controllers;
+
+use App\User;
+
 class UserController extends Controller
 {
     public function index()
     {
-        // ただの変数定義ですが、ここでModelにデータの処理を行わせたりします（後述）。
-        $name = 'yamada taro';
-
-        // ここでuserビュー「user.blade.php」を呼び出し、データ「name」を渡している。
-        return view('user', ['name' => $name]);
+         // データの追加 emailの値はランダムな文字列を使用。「.」で文字列の連結
+         $email = substr(str_shuffle('abcdefghijklmnopqrstuvwxyz'), 0, 8) . '@yyyy.com';
+         User::insert(['name' => 'yamada taro', 'email' => $email, 'password' => 'xxxxxxxx']);
+         // 全データの取り出し
+         $users = User::all();
+         return view('user', ['users' => $users]); 
     }
 }
